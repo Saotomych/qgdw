e3cc50d7681ba5a21096258e08c15881e92a802e
Alex <sao@m.ru>
2011-04-29 13:39:22 +0400

    rename uc1698 to am160160 in main

--- drivers/am160160_drv.c
+++ drivers/am160160_drv.c
@@ -29,8 +29,8 @@
 #undef CONFIG_PCI
 #undef CONFIG_PM
 
-#define UC1698CMD		AT91_CHIPSELECT_0		// 0x10000000
-#define UC1698DATA		(AT91_CHIPSELECT_0 | (1 << 19))
+#define am160160CMD		AT91_CHIPSELECT_0		// 0x10000000
+#define am160160DATA		(AT91_CHIPSELECT_0 | (1 << 19))
 
 #define PIXMAP_SIZE	1
 #define BUF_LEN		160*160
@@ -39,28 +39,28 @@ static unsigned char video[BUF_LEN];
 static PAMLCDFUNC hard;
 
 /* Board depend */
-static struct resource uc1698_resources[]={
+static struct resource am160160_resources[]={
 		[0]={
-			.start	= UC1698CMD,
-			.end 	= UC1698CMD + 1,
+			.start	= am160160CMD,
+			.end 	= am160160CMD + 1,
 			.flags 	= IORESOURCE_MEM,
 		},
 		[1] = {
-			.start 	= UC1698DATA,
-			.end 	= UC1698DATA + BUF_LEN,
+			.start 	= am160160DATA,
+			.end 	= am160160DATA + BUF_LEN,
 			.flags 	= IORESOURCE_MEM,
 		},
 };
 
-static struct platform_device uc1698_device = {
+static struct platform_device am160160_device = {
 		.name	= "uc6198",
 		.id 	= 0,
-		.num_resources	= ARRAY_SIZE(uc1698_resources),
-		.resource		= uc1698_resources,
+		.num_resources	= ARRAY_SIZE(am160160_resources),
+		.resource		= am160160_resources,
 };
 
 static struct platform_device *devices[] __initdata = {
-		&uc1698_device,
+		&am160160_device,
 };
 /* End Board depend */
 
@@ -69,15 +69,15 @@ static struct platform_device *devices[] __initdata = {
  * Driver data
  */
 static char *mode_option __initdata;
-struct uc1698_par;
-static unsigned char *uc1698_cmd = (unsigned char *) UC1698CMD;		// phys i/o indicator addresses
-static unsigned char *uc1698_data = (unsigned char *) UC1698DATA;
+struct am160160_par;
+static unsigned char *am160160_cmd = (unsigned char *) am160160CMD;		// phys i/o indicator addresses
+static unsigned char *am160160_data = (unsigned char *) am160160DATA;
 static unsigned char *io_cmd, *io_data;								// virtual i/o indicator addresses
 
 static int device_cnt=0;
 
-static struct fb_fix_screeninfo uc1698_fb_fix __devinitdata = {
-	.id =		"uc1698_fb",
+static struct fb_fix_screeninfo am160160_fb_fix __devinitdata = {
+	.id =		"am160160_fb",
 //	.type =		FB_TYPE_PACKED_PIXELS,
 	.type =		FB_TYPE_TEXT,
 	.visual =	FB_VISUAL_MONO01,
@@ -88,11 +88,11 @@ static struct fb_fix_screeninfo uc1698_fb_fix __devinitdata = {
 };
 
 //static struct fb_info info;
-//static struct uc1698__par __initdata current_par;
+//static struct am160160__par __initdata current_par;
 
-//int uc1698_fb_init(void);
+//int am160160_fb_init(void);
 
-static int uc1698_fb_open(struct fb_info *info, int user)
+static int am160160_fb_open(struct fb_info *info, int user)
 {
 	printk(KERN_INFO "fb_open(%d)\n",user);
 
@@ -100,7 +100,7 @@ static int uc1698_fb_open(struct fb_info *info, int user)
 }
 
 // (struct fb_info *info, char __user *buf,  size_t count, loff_t *ppos);
-static ssize_t uc1698_fb_read(struct fb_info *info, char __user *buffer, size_t length, loff_t *offset)
+static ssize_t am160160_fb_read(struct fb_info *info, char __user *buffer, size_t length, loff_t *offset)
 {
 	unsigned int len=0, i;
 	unsigned char *prddata = 0;
@@ -114,7 +114,7 @@ static ssize_t uc1698_fb_read(struct fb_info *info, char __user *buffer, size_t 
 	return len;
 }
 
-static ssize_t uc1698_fb_write(struct fb_info *info, const char __user *buffer, size_t length, loff_t *offset)
+static ssize_t am160160_fb_write(struct fb_info *info, const char __user *buffer, size_t length, loff_t *offset)
 {
     int rlen = info->fix.smem_len;
 //    unsigned long pos = (unsigned long) offset;
@@ -146,27 +146,27 @@ static ssize_t uc1698_fb_write(struct fb_info *info, const char __user *buffer, 
 	return rlen;
 }
 
-static int uc1698_fb_release(struct fb_info *info, int user)
+static int am160160_fb_release(struct fb_info *info, int user)
 {
 	printk(KERN_INFO "fb_release(%d)\n",user);
 
 	return 0;
 }
 
-//static int uc1698_fb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
+//static int am160160_fb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
 //{
 //    /* ... */
 //    return 0;	   	
 //}
 //
-//static int uc1698_fb_set_par(struct fb_info *info)
+//static int am160160_fb_set_par(struct fb_info *info)
 //{
-//    struct uc1698__par *par = info->par;
+//    struct am160160__par *par = info->par;
 //    /* ... */
 //    return 0;	
 //}
 //
-//static int uc1698_fb_setcolreg(unsigned regno, unsigned red, unsigned green,
+//static int am160160_fb_setcolreg(unsigned regno, unsigned red, unsigned green,
 //			   unsigned blue, unsigned transp,
 //			   struct fb_info *info)
 //{
@@ -217,40 +217,40 @@ static int uc1698_fb_release(struct fb_info *info, int user)
 //    return 0;
 //}
 //
-//static int uc1698_fb_pan_display(struct fb_var_screeninfo *var,
+//static int am160160_fb_pan_display(struct fb_var_screeninfo *var,
 //			     struct fb_info *info)
 //{
 //    return 0;
 //}
 //
-//static int uc1698_fb_blank(int blank_mode, struct fb_info *info)
+//static int am160160_fb_blank(int blank_mode, struct fb_info *info)
 //{
 //    /* ... */
 //    return 0;
 //}
 
 /* ------------ Accelerated Functions --------------------- */
-//void uc1698_fb_fillrect(struct fb_info *p, const struct fb_fillrect *region)
+//void am160160_fb_fillrect(struct fb_info *p, const struct fb_fillrect *region)
 //{
 //}
 //
-//void uc1698_fb_copyarea(struct fb_info *p, const struct fb_copyarea *area) 
+//void am160160_fb_copyarea(struct fb_info *p, const struct fb_copyarea *area)
 //{
 //}
 //
 //
-//void uc1698_fb_imageblit(struct fb_info *p, const struct fb_image *image) 
+//void am160160_fb_imageblit(struct fb_info *p, const struct fb_image *image)
 //{
 //}
 //
-//int uc1698_fb_cursor(struct fb_info *info, struct fb_cursor *cursor)
+//int am160160_fb_cursor(struct fb_info *info, struct fb_cursor *cursor)
 //{
 //}
-//void uc1698_fb_rotate(struct fb_info *info, int angle)
+//void am160160_fb_rotate(struct fb_info *info, int angle)
 //{
 //}
 //
-//int uc1698_fb_sync(struct fb_info *info)
+//int am160160_fb_sync(struct fb_info *info)
 //{
 //	return 0;
 //}
@@ -259,44 +259,44 @@ static int uc1698_fb_release(struct fb_info *info, int user)
      *  Frame buffer operations
      */
 
-static struct fb_ops uc1698_fb_ops = {
+static struct fb_ops am160160_fb_ops = {
 	.owner		= THIS_MODULE,
-	.fb_open	= uc1698_fb_open,
-	.fb_read	= uc1698_fb_read,
-	.fb_write	= uc1698_fb_write,
-	.fb_release	= uc1698_fb_release,
-//	.fb_check_var	= uc1698_fb_check_var,
-//	.fb_set_par	= uc1698_fb_set_par,
-//	.fb_setcolreg	= uc1698_fb_setcolreg,
-//	.fb_blank	= uc1698_fb_blank,
-//	.fb_pan_display	= uc1698_fb_pan_display,
+	.fb_open	= am160160_fb_open,
+	.fb_read	= am160160_fb_read,
+	.fb_write	= am160160_fb_write,
+	.fb_release	= am160160_fb_release,
+//	.fb_check_var	= am160160_fb_check_var,
+//	.fb_set_par	= am160160_fb_set_par,
+//	.fb_setcolreg	= am160160_fb_setcolreg,
+//	.fb_blank	= am160160_fb_blank,
+//	.fb_pan_display	= am160160_fb_pan_display,
 	.fb_fillrect	= cfb_fillrect, 	/* Needed !!! */
 	.fb_copyarea	= cfb_copyarea,	/* Needed !!! */
 	.fb_imageblit	= cfb_imageblit,	/* Needed !!! */
-//	.fb_cursor	= uc1698_fb_cursor,		/* Optional !!! */
-//	.fb_rotate	= uc1698_fb_rotate,
-//	.fb_sync	= uc1698_fb_sync,
-//	.fb_ioctl	= uc1698_fb_ioctl,
-//	.fb_mmap	= uc1698_fb_mmap,
+//	.fb_cursor	= am160160_fb_cursor,		/* Optional !!! */
+//	.fb_rotate	= am160160_fb_rotate,
+//	.fb_sync	= am160160_fb_sync,
+//	.fb_ioctl	= am160160_fb_ioctl,
+//	.fb_mmap	= am160160_fb_mmap,
 };
-//EXPORT_SYMBOL_GPL(uc1698_fb_read);
-//EXPORT_SYMBOL_GPL(uc1698_fb_write);
+//EXPORT_SYMBOL_GPL(am160160_fb_read);
+//EXPORT_SYMBOL_GPL(am160160_fb_write);
 
 /* ------------------------------------------------------------------------- */
 
     /*
      *  Initialization
      */
-/*static int __devinit uc1698_fb_probe(struct pci_dev *dev,
+/*static int __devinit am160160_fb_probe(struct pci_dev *dev,
 			      const struct pci_device_id *ent)*/
 
-static int uc1698_fb_probe (struct platform_device *pdev)	// -- for platform devs
+static int am160160_fb_probe (struct platform_device *pdev)	// -- for platform devs
 {
     struct device *dev = &pdev->dev;
     struct fb_info *info;
     unsigned char *pinfo;
-//    struct uc1698_info *sinfo;
-//    struct uc1698_info *pd_sinfo;
+//    struct am160160_info *sinfo;
+//    struct am160160_info *pd_sinfo;
 //    struct resource *map = NULL;
 //    struct fb_var_screeninfo *var;
     int cmap_len, retval;	
@@ -313,28 +313,28 @@ static int uc1698_fb_probe (struct platform_device *pdev)	// -- for platform dev
     }
 
     info->screen_base = (char __iomem *) video;
-    info->fbops = &uc1698_fb_ops;
+    info->fbops = &am160160_fb_ops;
 
 	mode_option = "160x160@60";
 
     retval = fb_find_mode(&info->var, info, mode_option, NULL, 0, NULL, 8);
     if (!retval || retval == 4) 	return -EINVAL;
-//    info->var = uc1698_fb_fix;
+//    info->var = am160160_fb_fix;
 
-    uc1698_fb_fix.smem_start = (unsigned long) video;
-    uc1698_fb_fix.smem_len = BUF_LEN >> 2;
-    info->fix = uc1698_fb_fix; /* this will be the only time uc1698_fb_fix will be
+    am160160_fb_fix.smem_start = (unsigned long) video;
+    am160160_fb_fix.smem_len = BUF_LEN >> 2;
+    info->fix = am160160_fb_fix; /* this will be the only time am160160_fb_fix will be
      	 	 	 	 	 	 	 * used, so mark it as __devinitdata
      	 	 	 	 	 	 	 */
-    uc1698_fb_fix.mmio_len = BUF_LEN >> 2;
-    io_data = uc1698_fb_fix.mmio_start = ioremap(uc1698_data, BUF_LEN >> 2);
-    io_cmd = ioremap(uc1698_cmd, 1);
+    am160160_fb_fix.mmio_len = BUF_LEN >> 2;
+    io_data = am160160_fb_fix.mmio_start = ioremap(am160160_data, BUF_LEN >> 2);
+    io_cmd = ioremap(am160160_cmd, 1);
 
     info->pseudo_palette = info->par;
     info->par = NULL;
     info->flags = FBINFO_FLAG_DEFAULT;
 
-    printk(KERN_INFO "set i/o sram: %lX+%lX; %lX+%lX\n", uc1698_fb_fix.mmio_start, uc1698_fb_fix.mmio_len, uc1698_fb_fix.smem_start, uc1698_fb_fix.smem_len);
+    printk(KERN_INFO "set i/o sram: %lX+%lX; %lX+%lX\n", am160160_fb_fix.mmio_start, am160160_fb_fix.mmio_len, am160160_fb_fix.smem_start, am160160_fb_fix.smem_len);
 
     cmap_len = 16;
     if (fb_alloc_cmap(&info->cmap, cmap_len, 0) < 0) return -ENOMEM;
@@ -359,8 +359,8 @@ static int uc1698_fb_probe (struct platform_device *pdev)	// -- for platform dev
 //     *  Cleanup
 //     */
 
- //static void __devexit uc1698_fb_remove(struct pci_dev *dev)
-static int __exit uc1698_fb_remove(struct platform_device *pdev)
+ //static void __devexit am160160_fb_remove(struct pci_dev *dev)
+static int __exit am160160_fb_remove(struct platform_device *pdev)
 {
 	struct fb_info *info = platform_get_drvdata(pdev);
 	/* or platform_get_drvdata(pdev); */
@@ -380,57 +380,57 @@ static int __exit uc1698_fb_remove(struct platform_device *pdev)
 
 #ifdef CONFIG_PM
 /**
- *	uc1698_fb_suspend - Optional but recommended function. Suspend the device.
+ *	am160160_fb_suspend - Optional but recommended function. Suspend the device.
  *	@dev: platform device
  *	@msg: the suspend event code.
  *
  *      See Documentation/power/devices.txt for more information
  */
-static int uc1698_fb_suspend(struct platform_device *dev, pm_message_t msg)
+static int am160160_fb_suspend(struct platform_device *dev, pm_message_t msg)
 {
 	struct fb_info *info = platform_get_drvdata(dev);
-	struct uc1698_fb_par *par = info->par;
+	struct am160160_fb_par *par = info->par;
 
 	/* suspend here */
 	return 0;
 }
 
 /**
- *	uc1698_fb_resume - Optional but recommended function. Resume the device.
+ *	am160160_fb_resume - Optional but recommended function. Resume the device.
  *	@dev: platform device
  *
  *      See Documentation/power/devices.txt for more information
  */
-static int uc1698_fb_resume(struct platform_dev *dev)
+static int am160160_fb_resume(struct platform_dev *dev)
 {
 	struct fb_info *info = platform_get_drvdata(dev);
-	struct uc1698_fb_par *par = info->par;
+	struct am160160_fb_par *par = info->par;
 
 	/* resume here */
 	return 0;
 }
 #else
-#define uc1698_fb_suspend NULL
-#define uc1698_fb_resume NULL
+#define am160160_fb_suspend NULL
+#define am160160_fb_resume NULL
 #endif /* CONFIG_PM */
 
-//static struct platform_device_driver uc1698_fb_driver = {
-static struct platform_driver uc1698_fb_driver = {
-//	.probe = uc1698_fb_probe,
-	.remove = __exit_p(uc1698_fb_remove),
+//static struct platform_device_driver am160160_fb_driver = {
+static struct platform_driver am160160_fb_driver = {
+//	.probe = am160160_fb_probe,
+	.remove = __exit_p(am160160_fb_remove),
 
 #ifdef CONFIG_PM
-	.suspend = uc1698_fb_suspend, /* optional but recommended */
-	.resume = uc1698_fb_resume,   /* optional but recommended */
+	.suspend = am160160_fb_suspend, /* optional but recommended */
+	.resume = am160160_fb_resume,   /* optional but recommended */
 #endif /* CONFIG_PM */
 
 	.driver = {
-		.name = "uc1698_fb",
+		.name = "am160160_fb",
 		.owner = THIS_MODULE,
 	},
 };
 
-static struct platform_device *uc1698_fb_device;
+static struct platform_device *am160160_fb_device;
 
 #ifndef MODULE
     /*
@@ -441,18 +441,18 @@ static struct platform_device *uc1698_fb_device;
  * Only necessary if your driver takes special options,
  * otherwise we fall back on the generic fb_setup().
  */
-int __init uc1698_fb_setup(char *options)
+int __init am160160_fb_setup(char *options)
 {
-    /* Parse user speficied options (`video=uc1698_fb:') */
+    /* Parse user speficied options (`video=am160160_fb:') */
 }
 #endif /* MODULE */
 
-static int __init uc1698_fb_init(void)
+static int __init am160160_fb_init(void)
 {
 	int ret;
 	unsigned char *pinfo;
 	/*
-	 *  For kernel boot options (in 'video=uc1698_fb:<options>' format)
+	 *  For kernel boot options (in 'video=am160160_fb:<options>' format)
 	 */
 	
 	/* Hardware initialize and testing */
@@ -483,25 +483,25 @@ static int __init uc1698_fb_init(void)
 #ifndef MODULE
 	char *option = NULL;
 
-	if (fb_get_options("uc1698_fb", &option))
+	if (fb_get_options("am160160_fb", &option))
 		return -ENODEV;
-	uc1698_fb_setup(option);
+	am160160_fb_setup(option);
 #endif
 
-	ret = platform_driver_probe(&uc1698_fb_driver, uc1698_fb_probe);
+	ret = platform_driver_probe(&am160160_fb_driver, am160160_fb_probe);
 	if (ret) {
 		// В случае когда девайс еще не добавлен
-		uc1698_fb_device = platform_device_alloc("uc1698_fb", 0);
-		if (uc1698_fb_device)
-			ret = platform_device_add(uc1698_fb_device);
+		am160160_fb_device = platform_device_alloc("am160160_fb", 0);
+		if (am160160_fb_device)
+			ret = platform_device_add(am160160_fb_device);
 		else{
 			device_cnt--;
 			ret = -ENOMEM;
 		}
 		if (ret) {
-			platform_device_put(uc1698_fb_device);
-			platform_driver_unregister(&uc1698_fb_driver);
-		}else ret = platform_driver_probe(&uc1698_fb_driver, uc1698_fb_probe); //uc1698_fb_probe(uc1698_fb_device);
+			platform_device_put(am160160_fb_device);
+			platform_driver_unregister(&am160160_fb_driver);
+		}else ret = platform_driver_probe(&am160160_fb_driver, am160160_fb_probe); //am160160_fb_probe(am160160_fb_device);
 	}
 
 	printk(KERN_INFO "device_open(%d)\n",ret);
@@ -509,14 +509,14 @@ static int __init uc1698_fb_init(void)
 	return ret;
 }
 
-static void __exit uc1698_fb_exit(void)
+static void __exit am160160_fb_exit(void)
 {
 	device_cnt--;
-	platform_device_unregister(uc1698_fb_device);
-	platform_driver_unregister(&uc1698_fb_driver);
+	platform_device_unregister(am160160_fb_device);
+	platform_driver_unregister(&am160160_fb_driver);
 	iounmap(io_cmd);
 	iounmap(io_data);
-	platform_device_del(&uc1698_device);
+	platform_device_del(&am160160_device);
 }
 
 /* ------------------------------------------------------------------------- */
@@ -526,7 +526,7 @@ static void __exit uc1698_fb_exit(void)
      *  Modularization
      */
 
-module_init(uc1698_fb_init);
-module_exit(uc1698_fb_exit);
+module_init(am160160_fb_init);
+module_exit(am160160_fb_exit);
 
 MODULE_LICENSE("GPL");
