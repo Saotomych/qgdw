67d11ab0c97dc9c3b5aaafd56e5281e0af6fe135
Grygorii Musiiaka <grymster@gmail.com>
2011-06-30 17:10:35 +0400

    structure ep_data_header and some constants added

--- common/common.h
+++ common/common.h
@@ -18,4 +18,35 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+/*
+ *
+ * Constants and byte flags/masks
+ *
+ */
+#define EP_USER_DATA			0		/* user data */
+#define EP_MSG_INIT_CFG			1		/* initialization configuration */
+#define EP_MSG_INIT_ACK			2		/* initialization acknowledgment */
+#define EP_MSG_INIT_NACK		3		/* initialization negative acknowledgment */
+#define EP_MSG_RECONNECT		4		/* re-connection request */
+#define EP_MSG_CONNECT_ACK		5		/* connection acknowledgment */
+#define EP_MSG_CONNECT_NACK		6		/* connection negative acknowledgment */
+#define EP_MSG_CONNECT_CLOSE	7		/* connection close request */
+#define EP_MSG_CONNECT_LOST		8		/* connection lost/closed by remote peer */
+
+
+
+/*
+ *
+ * Structures
+ *
+ */
+
+typedef struct ep_data_header {
+	uint16_t 		adr;		/* link (ASDU) address */
+	uint32_t		sys_msg;	/* user data (0)/system message(1..n) */
+	uint32_t		len;		/* user data length */
+} ep_data_header;
+
+
+
 #endif /* COMMON_H_ */
