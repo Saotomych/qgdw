c9e67fe7e74e8ee262c8da542ac59376630c8f90
Grygorii Musiiaka <grymster@gmail.com>
2011-07-01 18:13:30 +0400

    byte array function added

--- common/asdu.c
+++ common/asdu.c
@@ -5,6 +5,7 @@
  */
  
 #include <malloc.h>
+#include <string.h>
 #include "asdu.h"
 
 
@@ -46,6 +47,64 @@ void asdu_cleanup_data(asdu *unit)
 	unit->data = NULL;
 
 	unit->size = 0;
+}
+
+
+
+uint8_t asdu_from_byte(unsigned char *buff, uint32_t buff_len, asdu **unit)
+{
+	// fast check input data
+	if(buff_len < sizeof(asdu) || !unit) return RES_ASDU_INCORRECT;
+
+	*unit = asdu_create();
+
+	memcpy((void*)(*unit), (void*)buff, sizeof(asdu));
+
+	if((*unit)->size == 0 || buff_len < sizeof(asdu) + (*unit)->size * sizeof(data_unit))
+	{
+		asdu_destroy(unit);
+		return RES_ASDU_INCORRECT;
+	}
+
+	(*unit)->data = NULL;
+
+	(*unit)->data = (data_unit*) malloc((*unit)->size * sizeof(data_unit));
+
+	if(!(*unit)->data)
+	{
+		asdu_destroy(unit);
+		return RES_ASDU_MEM_ALLOC;
+	}
+
+	memcpy((void*)(*unit)->data, (void*)(buff + sizeof(asdu)), (*unit)->size * sizeof(data_unit));
+
+	return RES_ASDU_SUCCESS;
+}
+
+
+uint8_t asdu_to_byte(unsigned char **buff, uint32_t *buff_len, asdu *unit)
+{
+	// fast check input data
+	if(!buff || !unit || !unit->data ) return RES_ASDU_INCORRECT;
+
+	// calculate buffer length
+	*buff_len = sizeof(asdu) + unit->size * sizeof(data_unit);
+
+	// allocate memory for the buffer
+	*buff = (unsigned char*) malloc(*buff_len);
+
+	if(!*buff)
+	{
+		*buff_len = 0;
+
+		return RES_ASDU_MEM_ALLOC;
+	}
+
+	memcpy((void*)(*buff), (void*)unit, sizeof(asdu));
+
+	memcpy((void*)(*buff + sizeof(asdu)), (void*)unit->data, unit->size * sizeof(data_unit));
+
+	return RES_ASDU_SUCCESS;
 }
 
 
