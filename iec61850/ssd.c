4b59a3870abd8a4cde59e1e2af3f0f3296f3f1e2
Alex <sao@m.ru>
2011-07-13 14:30:00 +0400

    add sdd.c: set functions of parsing *.ssd (SCL file of iec61850)

---
+++ iec61850/ssd.c
@@ -0,0 +1,30 @@
+/*
+ * ssd.c
+ *
+ *  Created on: 13.07.2011
+ *      Author: alex
+ */
+
+int ssd_create_ied(char* cstr){			// call parse ied
+
+}
+
+int ssd_create_ln(char* cstr){			// call parse ln
+
+}
+
+int ssd_create_data(char* cstr){			// call parse data
+
+}
+
+int ssd_create_datatype(char* cstr){		// call parse data_type
+
+}
+
+int ssd_create_enum(char* cstr){			// call parse enum
+
+}
+
+int ssd_create_subst(char* cstr){			// call parse substation
+
+}
