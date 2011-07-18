f9910ad2c426a2d0a900e2ee5b6a935c2c312342
Grygorii Musiiaka <grymster@gmail.com>
2011-07-18 15:19:14 +0400

    share time functions

--- unitlinks/include/dlt_asdu.h
+++ unitlinks/include/dlt_asdu.h
@@ -39,6 +39,10 @@ uint8_t dlt_asdu_buff_parse(unsigned char *buff, uint32_t buff_len, asdu *dlt_as
 /* build buffer from given ASDU structure */
 uint8_t dlt_asdu_buff_build(unsigned char **buff, uint32_t *buff_len, asdu *dlt_asdu);
 
+time_t dlt_asdu_parse_time_tag(unsigned char *buff, uint32_t *offset);
+void dlt_asdu_build_time_tag(unsigned char *buff, uint32_t *offset, time_t time_tag);
+
+
 
 #ifdef __cplusplus
 }
