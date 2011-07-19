ade5d2010a854face2a5e0fa12cf662b23c98435
Alex <sao@m.ru>
2011-07-19 17:13:24 +0400

    change 2th par of call mf_newep

--- unitlinks/src/iec104.c
+++ unitlinks/src/iec104.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 			if (ret == 1){
 	    	// start forward endpoint
 				printf("Unit-IEC104: forward endpoint\n");
-				mf_newendpoint(&cd, "/rw/mx00/phyints", 1);
+				mf_newendpoint(&cd, CHILD_APP_PATH, 1);
 				printf("%s: System message EP_MSG_CONNECT sent. Address = %d\n", APP_NAME, 967);
 				iec104_sys_msg_send(EP_MSG_CONNECT, cd.addr, DIRDN);
 			}
