26b2ec88b26e45bc62f4e01a7864314c157aa3fe
Grygorii Musiiaka <grymster@gmail.com>
2011-09-01 13:41:00 +0400

    debug messages improved

--- unitlinks/src/iec104.c
+++ unitlinks/src/iec104.c
@@ -192,12 +192,15 @@ void iec104_catch_alarm(int sig)
 			// check timer t0
 			if(ep_exts[i]->timer_t0 > 0 && difftime(cur_time, ep_exts[i]->timer_t0) >= t_t0)
 			{
+#ifdef _DEBUG
+				printf("%s: Timer t0 went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
+#endif
+
 				iec104_init_ep_ext(ep_exts[i]);
 
 				iec104_sys_msg_send(EP_MSG_RECONNECT, ep_exts[i]->adr, DIRDN, NULL, 0);
 
 #ifdef _DEBUG
-				printf("%s: Timer t0 went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 				printf("%s: System message EP_MSG_RECONNECT sent. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 #endif
 			}
@@ -206,12 +209,15 @@ void iec104_catch_alarm(int sig)
 			// check timer t1
 			if(ep_exts[i]->timer_t1 > 0 && difftime(cur_time, ep_exts[i]->timer_t1) >= t_t1)
 			{
+#ifdef _DEBUG
+				printf("%s: Timer t1 went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
+#endif
+
 				iec104_init_ep_ext(ep_exts[i]);
 
 				iec104_sys_msg_send(EP_MSG_RECONNECT, ep_exts[i]->adr, DIRDN, NULL, 0);
 
 #ifdef _DEBUG
-				printf("%s: Timer t1 went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 				printf("%s: System message EP_MSG_RECONNECT sent. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 #endif
 			}
@@ -238,12 +244,15 @@ void iec104_catch_alarm(int sig)
 			// check timer rc
 			if(ep_exts[i]->timer_rc > 0 && difftime(cur_time, ep_exts[i]->timer_rc) >= t_rc)
 			{
+#ifdef _DEBUG
+				printf("%s: Timer rc went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
+#endif
+
 				iec104_init_ep_ext(ep_exts[i]);
 
 				iec104_sys_msg_send(EP_MSG_RECONNECT, ep_exts[i]->adr, DIRDN, NULL, 0);
 
 #ifdef _DEBUG
-				printf("%s: Timer rc went off. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 				printf("%s: System message EP_MSG_RECONNECT sent. Address = %d.\n", APP_NAME, ep_exts[i]->adr);
 #endif
 			}
