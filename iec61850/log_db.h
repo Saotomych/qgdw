7582db5c27d01b640b96b2f7bc1d52b32068e13f
Grygorii Musiiaka <grymster@gmail.com>
2012-03-26 13:07:23 +0400

    some log algorithms improved

--- iec61850/log_db.h
+++ iec61850/log_db.h
@@ -25,29 +25,27 @@ extern "C" {
  *
  */
 
-#define LOG_FLD_NUM			25
-#define LOG_SERV_PRM_SIZE	2
-
-
 /*
  *
  * Structures
  *
  */
 
-typedef struct log_db_serv_prm {
-	char			*db_name;
-	DB				*db_req;
-	char			**db_flds;
-	int				flds_num;
-	const time_t	add_period;
-	time_t			add_timer;
-	const time_t	export_period;
-	time_t			export_timer;
-	const time_t	storage_deep;
-} log_db_serv_prm;
+typedef struct log_db log_db;
 
-extern log_db_serv_prm log_serv_prm_list[LOG_SERV_PRM_SIZE];
+struct log_db {
+	DB		*db;
+	char	**flds;
+	int		flds_num;
+	time_t	add_period;
+	time_t	add_timer;
+	time_t	export_period;
+	time_t	export_timer;
+	time_t	storage_deep;
+	void	(*add_var_rec)(log_db *db_req, uint32_t adr, uint32_t *log_rec);
+	int		(*get_var)(log_db *db_req, uint32_t adr, char *var_name, int len, time_t *log_time, uint32_t *value);
+	void	(*add_event)(log_db *db_req, uint32_t adr, char *msg, int len);
+};
 
 
 /*
@@ -58,17 +56,9 @@ extern log_db_serv_prm log_serv_prm_list[LOG_SERV_PRM_SIZE];
 int log_db_env_open();
 void log_db_env_close();
 
-void log_db_init_timers();
+void log_db_init_timers(time_t init_time);
 
-int log_db_get_fld_idx(char *var_name);
-
-void log_db_add_var_rec(uint32_t adr, uint32_t *log_rec, DB *db_req, char **log_flds, int fld_num);
-
-int log_db_export_data(DB *db_req, time_t cut_time, char *db_file_out);
-
-int log_db_clean_old_data(DB *db_req, time_t cut_time, time_t storage_deep);
-
-
+void log_db_maintain_databases(time_t cut_time);
 
 
 #ifdef __cplusplus
