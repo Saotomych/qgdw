34b2bab1e40761c574fc3ab075b7f312a6173f4b
Alex <sao@m.ru>
2012-03-26 16:35:42 +0400

    IEC: correct counter get_next_dataunit

--- iec61850/virtualize.c
+++ iec61850/virtualize.c
@@ -560,7 +560,7 @@ asdu *pasdu = (asdu*) (edh + 1);
 }
 
 data_unit *get_next_dataunit(data_unit *pdu, ep_data_header *edh){
-int32_t len = (uint32_t) pdu - (uint32_t) edh - edh->len + sizeof(data_unit);
+int32_t len = (uint32_t) pdu - (uint32_t) edh - edh->len - sizeof(ep_data_header);
 
 	pdu++;
 	if (len < 0) return pdu;
